803c2e75c65817e2aa6bfd426456fc68a25aaa09
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-26 18:46:47 -0300

    criando os primeiros componentes

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
+import HeaderHome from './HeaderHome';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <HeaderHome />
       </div>
     );
   }

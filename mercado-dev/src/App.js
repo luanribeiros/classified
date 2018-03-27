b291ad0a2b92c6e63914bde8459d1b85bbf558f7
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-26 22:12:16 -0300

    passando argumentos com props

--- mercado-dev/src/App.js
+++ mercado-dev/src/App.js
@@ -19,7 +19,12 @@ class App extends Component {
             <AnuncioHome />
           </div>
           <h3> Categorias </h3>
-          <LinkCategoria />
+          <div className="row">
+          <LinkCategoria categoria={ { categoria: 'Carros', icon: 'fa-lightbulb-o' } } />
+          <LinkCategoria categoria={ { categoria: 'Carros', icon: 'fa-lightbulb-o' } } />
+          <LinkCategoria categoria={ { categoria: 'Carros', icon: 'fa-lightbulb-o' } } />
+          <LinkCategoria categoria={ { categoria: 'Carros', icon: 'fa-lightbulb-o' } } />
+          </div>
         </div>
         <Footer />
       </div>

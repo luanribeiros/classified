7243b6161fa0e86d042f6982e0f99398b7e74ddd
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-27 01:03:09 -0300

    novas implementações no componente NovoAnuncio

--- src/NovoAnuncio.js
+++ src/NovoAnuncio.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 class NovoAnuncio extends Component {
   render() {
     return (
-      <h1> Novo Anúncio </h1>
+      <div>
+        <div className='container' style={{ paddingTop: '120px'}}>
+          <h1></h1>
+          <form onSubmit={null}>
+            <div className='form-group'>
+              <label htmlFor='nome'>Nome</label>
+              <input type='text' className='form-control' id='nome'placeholder='Nome' />
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }

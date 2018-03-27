48dee1ec48dad46b6ca95d6f140e0791f55ea1f0
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-27 01:26:10 -0300

    referenciando o handleSubmit com this

--- src/NovoAnuncio.js
+++ src/NovoAnuncio.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import HeaderInterno from './HeaderInterno'
 
 class NovoAnuncio extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit() {
+    return false;
+  }
+
   render() {
     return (
       <div>
         <HeaderInterno />
         <div className='container' style={{ paddingTop: '120px'}}>
           <h1>Novo Anúncio</h1>
-          <form onSubmit={null}>
+          <form onSubmit={this.handleSubmit}>
             <div className='form-group'>
               <label htmlFor='nome'>Nome</label>
-              <input type='text' className='form-control' id='nome' placeholder='Nome' />
+              <input type='text' className='form-control' id='nome' placeholder='Nome'
+                     ref={ (ref) => this.nome = ref } />
             </div>
             <div className='form-group'>
               <label htmlFor='nome'>Descrição</label>

72c4eb81c3197e44d78a6753568dd45645e34917
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-27 01:09:53 -0300

    aplicando a logo em NovoAnuncio

--- src/HeaderInterno.js
+++ src/HeaderInterno.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo2 from './logo2.png'
 
 const HeaderInterno = (props) => {
   return (
     <nav className="navbar navbar-dark fixed-top bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/"><img alt="MercadoDev" src={null} height={80} /></Link>
+        <Link className="navbar-brand" to="/"><img alt="MercadoDev" src={logo2} height={80} /></Link>
         <div className="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">

a21e121c767ce5324c77e4663ec2ba49cc9ebd07
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-27 00:42:01 -0300

    criando novo compoente Home e colocando suas propriedades devidas

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
 
 import HeaderHome from './HeaderHome'
-import AnuncioHome from './AnuncioHome'
 import Footer from './Footer'
-import LinkCategoria from './LinkCategoria'
-import base from './base'
+import Home from './Home'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      categorias: [],
-      anuncios: []
-    }
-    base.bindToState('categorias', {
-      context: this,
-      state: 'categorias'
-    })
-    base.bindToState('anuncios', {
-      context: this,
-      state: 'anuncios',
-      queries: {
-        limitToLast: 3
-      }
-    })
-  }
   render() {
-    let index = 0;
     return (
       <div className="App">
         <HeaderHome />
-        <div className="container">
-          <h3> Últimos Anúncios </h3>
-          <div className="row">
-          { this.state.anuncios.map( (anuncio, indice) => {
-            return <AnuncioHome key={indice} anuncio={anuncio} />
-          })}
-          </div>
-          <h3> Categorias </h3>
-          <div className="row">
-          { this.state.categorias.map( (cat, indice) => {
-            return [
-              <LinkCategoria categoria={cat} key={indice} />,
-              ++index % 4 === 0 && <div key={'c'+indice} className="w-100"></div>
-            ]
-          })}
-          </div>
-        </div>
+        <Home />
         <Footer />
       </div>
     );

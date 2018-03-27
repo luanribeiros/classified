62b6f8ce18aff28727f195188df41eaea0ab9df1
Luan Ribeiro <luanribeiros@gmail.com>
2018-03-26 22:18:23 -0300

    criando arquivo para o firebase

---
+++ mercado-dev/src/base.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyANX58gcaimXSAAUZw4MhX_2AwYbC8-c9o",
+  authDomain: "mercadodev-867c6.firebaseapp.com",
+  databaseURL: "https://mercadodev-867c6.firebaseio.com",
+  projectId: "mercadodev-867c6",
+  storageBucket: "",
+  messagingSenderId: "797108387050"
+};

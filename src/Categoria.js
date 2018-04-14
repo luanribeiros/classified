f219062e9a2695183213a2656f66c11048cce26b
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-14 02:29:43 -0300

    transformando o componente Categoria para Statefull

--- src/Categoria.js
+++ src/Categoria.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Categoria = (props) => {
-  return (
-    <h1> Categoria: {JSON.stringify(props.match.params.urlCategoria)} </h1>
-  );
+// https://mercadodev-867c6.firebaseio.com/anuncios.json?orderBy=%22categoria%22&equalTo=%22esportes-e-lazer%22
+
+class Categoria extends Component {
+  render() {
+    return (
+      <h1> Categoria: {JSON.stringify(props.match.params.urlCategoria)} </h1>
+    );
+  }
 }
-
 export default Categoria

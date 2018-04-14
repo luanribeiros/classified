4654c5435c64b7c3906d8ce67699ae84f11141b2
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-13 23:04:06 -0300

    mapeando as categorias e retornando baseado nas categorias

--- src/Categorias.js
+++ src/Categorias.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeaderInterno from './HeaderInterno';
+import { Link } from 'react-router-dom'
 
 const Categorias = (props) => {
   return (
@@ -9,7 +10,14 @@ const Categorias = (props) => {
         <h1> Categorias </h1>
         <div className="row">
           <div className="col-lg-4">
-           Opaaa
+            {props.categorias.map(
+              cat => {
+                return (
+                  <li><Link to={`/categorias/${cat.url}`}> {cat.categoria} </Link></li>
+                )
+              }
+            )}
+            {JSON.stringify(props.categorias)}
           </div>
           <div className="col-lg-8">
           COnteudoooo

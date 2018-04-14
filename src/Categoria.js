2a78d7a7b6266e756bd4399b1f8a7295091c3df7
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-14 02:32:20 -0300

    instalando o axios

--- src/Categoria.js
+++ src/Categoria.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 // https://mercadodev-867c6.firebaseio.com/anuncios.json?orderBy=%22categoria%22&equalTo=%22esportes-e-lazer%22
 
 class Categoria extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     return (
-      <h1> Categoria: {JSON.stringify(props.match.params.urlCategoria)} </h1>
+      <h1>
+        Categoria:
+        {JSON.stringify(this.props.match.params.urlCategoria)}
+      </h1>
     );
   }
 }

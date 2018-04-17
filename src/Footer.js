4ad301643c31cbe17b74b9e454215054941c8f2c
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-16 22:03:01 -0300

    ajustes Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,9 @@
 import React from  'react'
 
-const Footer = (props) => {
-  return (
+export default props => (
     <footer className="py-5 bg-dark mt-4">
       <div className="container">
         <p className="m-0 text-center text-white">Copyright &copy; DevPleno.com 2017</p>
       </div>
     </footer>
   );
-}
-
-export default Footer
